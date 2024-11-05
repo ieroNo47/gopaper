e5c010a1cf767c248556f1ce50e804ae2859ee96
alex-ieronymos <alexandros.ieronymos@sap.com>
2024-11-05 10:14:02 +0000

    oauth check in python

--- prototyping/test.py
+++ prototyping/test.py
@@ -7,14 +7,59 @@ import requests
 
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
+from requests_oauthlib import OAuth1, OAuth1Session
+from urllib.parse import parse_qs
 
 
-def auth_check(s, api):
+def basic_auth_check():
+    username = os.environ.get("IP_USER")
+    password = os.environ.get("IP_PASSWORD")
+    api = os.environ.get("IP_API")
     url = f"{api}/authenticate"
-    r = s.get(url)
+    r = requests.get(url, auth=HTTPBasicAuth(username, password))
     try:
         r.raise_for_status()
-        print(f"OK: {r.status_code}")
+        print(f"BASIC AUTH: OK: {r.status_code}")
+    except requests.exceptions.HTTPError as he:
+        print(f"FAILED: {he}")
+        sys.exit(1)
+
+
+def oauth_check():
+    consumer_key = os.environ.get("IP_OAUTH_CONSUMER_ID")
+    consumer_secret = os.environ.get("IP_OAUTH_CONSUMER_SECRET")
+    username = os.environ.get("IP_USER")
+    password = os.environ.get("IP_PASSWORD")
+    api = os.environ.get("IP_API")
+    oauth_consumer = OAuth1(consumer_key, client_secret=consumer_secret)
+    url = f"{api}/1.1/oauth/access_token"
+    params = {
+        "x_auth_username": username,
+        "x_auth_password": password,
+        "x_auth_mode": "client_auth"
+    }
+    r = requests.post(url, auth=oauth_consumer, params=params)
+    try:
+        r.raise_for_status()
+        print(f"ACCESS TOKEN: OK: {r.status_code}")
+    except requests.exceptions.HTTPError as he:
+        print(f"FAILED: {he}")
+        sys.exit(1)
+    token_data = parse_qs(r.text)
+    oauth_token_secret = token_data.get("oauth_token_secret")[0]
+    oauth_token = token_data.get("oauth_token")[0]
+    oauth_owner = OAuth1(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=oauth_token,
+        resource_owner_secret=oauth_token_secret
+    )
+    url = f"{api}/1.1/account/verify_credentials"
+    r = requests.post(url, auth=oauth_owner)
+    try:
+        r.raise_for_status()
+        print(f"OAUTH: OK: {r.status_code}")
+        print(r.json())
     except requests.exceptions.HTTPError as he:
         print(f"FAILED: {he}")
         sys.exit(1)
@@ -22,22 +67,8 @@ def auth_check(s, api):
 
 def main():
     load_dotenv()
-    username = os.environ.get("IP_USER")
-    password = os.environ.get("IP_PASSWORD")
-    api = os.environ.get("IP_API")
-    s = requests.Session()
-    s.auth = HTTPBasicAuth(username, password)
-    auth_check(s, api)
-
-    url = f"{api}/1/oauth/access_token"
-    tr = s.post(url, params={
-        "x_auth_username": username,
-        "x_auth_password": password,
-        "x_auth_mode": "client_auth"
-    })
-    print(tr.text)
-
-
+    basic_auth_check()
+    oauth_check()
 
 
 if __name__ == "__main__":
