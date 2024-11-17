fb5681eb7da4daeaf01c728b01bfd85475082d52
Alex Iero <a.ieronimos@gmail.com>
2024-11-17 18:14:44 +0000

    authorized request

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/ieroNo47/gopaper
 
 go 1.23.1
 
-require github.com/joho/godotenv v1.5.1
-
-require github.com/dghubble/oauth1 v0.7.3 // indirect
+require (
+	github.com/dghubble/oauth1 v0.7.3
+	github.com/joho/godotenv v1.5.1
+)
---
+++ prototyping/ip-models.go
@@ -0,0 +1,57 @@
+// Instapaper models
+package main
+
+type Response struct {
+	Highlights []Highlight `json:"highlights"` // empty array
+	Bookmarks  []Bookmark  `json:"bookmarks"`
+	User       User        `json:"user"`
+}
+
+type Highlight struct {
+	HighlightID int64       `json:"highlight_id"`
+	Text        string      `json:"text"`
+	Note        interface{} `json:"note"` // using interface{} since it can be null
+	BookmarkID  int64       `json:"bookmark_id"`
+	Time        int64       `json:"time"`
+	Position    int         `json:"position"`
+	Type        string      `json:"type"`
+}
+
+type Bookmark struct {
+	Hash              string  `json:"hash"`
+	Description       string  `json:"description"`
+	Tags              []Tag   `json:"tags"`
+	BookmarkID        int64   `json:"bookmark_id"`
+	PrivateSource     string  `json:"private_source"`
+	Title             string  `json:"title"`
+	URL               string  `json:"url"`
+	ProgressTimestamp int64   `json:"progress_timestamp"`
+	Time              int64   `json:"time"`
+	Progress          float64 `json:"progress"`
+	Starred           string  `json:"starred"`
+	Type              string  `json:"type"`
+}
+
+type Tag struct {
+	Count int     `json:"count"`
+	Hash  string  `json:"hash"`
+	Name  string  `json:"name"`
+	ID    int     `json:"id"`
+	Time  float64 `json:"time"`
+	Slug  string  `json:"slug"`
+}
+
+type User struct {
+	Username             string `json:"username"`
+	UserID               int    `json:"user_id"`
+	Type                 string `json:"type"`
+	SubscriptionIsActive string `json:"subscription_is_active"`
+}
+
+func (r Response) getBookmarkTitles() []string {
+	titles := []string{}
+	for _, bookmark := range r.Bookmarks {
+		titles = append(titles, bookmark.Title)
+	}
+	return titles
+}
